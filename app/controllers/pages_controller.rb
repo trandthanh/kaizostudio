f0849246aa6f9466d5465c950094a8c15c5665eb
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-09-04 09:25:29 +0200

    Created correct routes for static pages (home, contact, agence) + Finished index action for the Description model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :agence, :contact]
 
   def home
+  end
+
+  def agence
+  end
+
+  def contact
   end
 end
--- app/controllers/realisations_controller.rb
+++ app/controllers/realisations_controller.rb
@@ -1,2 +1,31 @@
 class RealisationsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @realisations = Realisation.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
