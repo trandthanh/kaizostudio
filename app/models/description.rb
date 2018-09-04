c544a378e29348762896004fa04f5576465d62f5
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-09-04 13:17:02 +0200

    Set up admin field on User model + Added one User as admin

--- app/models/description.rb
+++ app/models/description.rb
@@ -1,2 +1,8 @@
 class Description < ApplicationRecord
+  #belongs_to :user
+
+  validates :page, presence: true
+  validates :role, presence: true
+  validates :style, presence: true
+  validates :text, presence: true
 end
--- app/models/realisation.rb
+++ app/models/realisation.rb
@@ -1,3 +1,8 @@
 class Realisation < ApplicationRecord
+  #belongs_to :user
   has_many :galleries, dependent: :destroy
+
+  validates :title, presence: true, uniqueness: true
+  validates :subtitle, presence: true
+  validates :description, presence: true
 end
