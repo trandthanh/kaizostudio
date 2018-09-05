cebdad7c8c174cdc07791518bd0dd92403887fb3
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-09-05 10:22:18 +0200

    FRONT / Finished Upper part of home except link from banner picture to realisation project

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :agence, :contact]
 
   def home
-    @home_descriptions = Description.find_by(page: "Home")
+    @home_descriptions = Description.where(:page == "Home")
 
     @featured_realisations = Realisation.where(:featured => true)
   end
